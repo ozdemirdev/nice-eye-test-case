64fc86a33d90771830e9c7ad70fba6bbd6c00b19
ozdemirdev <ttarikozdemirdev@hotmail.com>
2024-01-25 15:36:04 +0300

    asdsa

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
     'linebreak-style': 0,
-    "quotes": 0,
+    'avoidEscape': true,
+    'allowTemplateLiterals': true,
     'import/extensions': ['error', {
       js: 'always',
     }],
